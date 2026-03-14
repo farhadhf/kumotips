a7c55d006c3d3187d39b34b0ea77974c2843c982
Farhad Hedayatifard <hf.farhad@gmail.com>
2026-03-14 17:01:17 +0100

    Update getRecordMap.ts

--- src/apis/notion-client/getRecordMap.ts
+++ src/apis/notion-client/getRecordMap.ts
@@ -1,9 +1,7 @@
 import { NotionAPI } from "notion-client"
 
 export const getRecordMap = async (pageId: string) => {
-  const api = new NotionAPI({
-    apiBaseUrl: "https://prong-waterfall-12c.notion.site/api/v3"
-  })
+  const api = new NotionAPI()
   const recordMap = await api.getPage(pageId)
   return recordMap
 }
